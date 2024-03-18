3b1633efdb63b0e889f77d9c721e7905d6254838
serrer6 <serrer6@outlook.com>
2024-03-18 23:00:58 +0800

    Change dynlantrd.js

--- src/dynlantrd.js
+++ src/dynlantrd.js
@@ -1,13 +1,24 @@
 export default class DynlantRD {
-	constructor(dom){
+	constructor(dom,themes){
 		if (!dom) {
-			console.error('Charts Missing parameters!')
-			return false
+			console.error('[DynlantRD]No selected element!');
+			return false;
 		}
 	}
-	static version = "1.0.1"
+	static version = "1.0.1";
+	//辨别json格式富文本的版本，对未来提供向下兼容性
+	static protocol = '1';
 	
 	static init(element,theme="default"){
-		return new DynlantRD(element)
+		return new DynlantRD(element);
 	}
+}
+
+/**
+ * @description JSON格式的富文本的渲染器
+ * @param {String} richtext          想要修改的配置项的key
+ * @return {Undefined} 无返回值
+ */
+DynlantRD.prototyp.RenderJSON = function(richtext){
+  var richtext_obj = JSON.parse(richtext);
 }
