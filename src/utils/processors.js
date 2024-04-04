84bb9a4c05a2a611e6d575821c333f68a0259d09
serrer6 <serrer6@outlook.com>
2024-04-04 14:18:02 +0800

    Change Processors.JS [Fix RenderElement Function]

--- src/utils/processors.js
+++ src/utils/processors.js
@@ -22,7 +22,7 @@ class Processors{
 		let element_dom = undefined;
 		let element_orig = this.element;
 		// 判断是不是Object
-		if(typeof element_obj && !isNaN(element_obj.length)){
+		if(typeof element_obj && isNaN(element_obj.length)){
 
 			// 创建元素
 			if(element_obj.hasOwnProperty("element") && typeof element_obj.element == "string"){
@@ -42,28 +42,24 @@ class Processors{
 			}
 
 			// 处理元素属性
-			if(element_obj.hasOwnProperty("attribute") && typeof element_obj.attribute == "object" && !isNaN(element_obj.length)){
-				let keys = Object.keys(element_obj.attribute);
-				for(let index=0;index<keys.lenght;index++){
-					if(keys[index] == "eclass"){
-						keys[index] = "class";
-					}
-				}
-				for(let index=0;index<keys.length;index++){
-
-					if(element_obj["attribute"].hasOwnProperty(keys[index]) && typeof element_obj["attribute"][keys[index]] == "string"){
-						element_dom.setAttribute(keys[index],element_obj["attribute"][keys[index]]);
-						this.OutDebug(`[Processors]Add Attribute(TEXT) ${keys[inded]} Vaule ${element_obj["attribute"][keys[index]]}`);
-					}else if(Array.isArray(element_obj["attribute"][keys[index]])){
-						let value = element_obj["attribute"][keys[index]];
-						let outvalue = "";
-						for(let tindex=0;tindex<value.lenght;tindex++){
-							outvalue += value[tindex] + " ";
-						}
+			if(element_obj.hasOwnProperty("attribute") && typeof element_obj.attribute == "object" && isNaN(element_obj.length)){
+				let orig_keys = Object.keys(element_obj.attribute);
+				Logger.info(orig_keys);
+				const rp = (data, obj) => {return data.map(item => obj[item] || item);}
+        let keys = rp(orig_keys,{eclass:'class'});
+				Logger.info(keys);
+				for(let index=0;index<orig_keys.length;index++){
+				  
+					if(element_obj.attribute.hasOwnProperty(orig_keys[index]) && !Array.isArray(element_obj.attribute[orig_keys[index]])){
+						element_dom.setAttribute(keys[index],element_obj.attribute[orig_keys[index]]);
+						this.OutDebug(`[Processors]Add Attribute(TEXT) ${keys[index]} Vaule ${element_obj.attribute[orig_keys[index]]}`);
+					}else{
+						let value = element_obj.attribute[orig_keys[index]];
+						let outvalue = value.join(' ');
 						element_dom.setAttribute(keys[index],outvalue);
-						this.OutDebug(`[Processors]Add Attribute(TEXT) ${keys[inded]} Vaule ${element_    obj["attribute"][keys[index]]}`);
+						this.OutDebug(`[Processors]Add Attribute(Array) ${keys[index]} Vaule ${outvalue}`);
 					}
-				}
+			  }
 			}
 
 			// 插入
